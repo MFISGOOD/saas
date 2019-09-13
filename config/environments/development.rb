49d2ed5f47365a3c2ec2c4ed6f3df8c1b56d6e73
MFISGOOD <turbot2@hotmail.com>
2019-09-13 15:15:57 +0200

    moved actionmaile settings from config/environments/development.rb to config/environments/development.rb to  config/environment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,12 +75,3 @@ Rails.application.configure do
 
 end
 
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :user_password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
-}
