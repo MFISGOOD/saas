ea81590580a3a799eeacf87a08c56e96780db128
MFISGOOD <turbot2@hotmail.com>
2019-09-26 22:14:08 +0200

    Modified devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_filter :configure_permitted_paramaters, if: :devise_controller?
+    protected
+    def configure_permitted_paramaters
+        devise_paramter_sanitizer.for(:sing_up){ |u| u.permit(:name,:stripe_card_token,:email,:password,:password_confirmation)}
+    end
 end
