0946461e35be1375d6fc75d5cd70b6ae753d7591
MFISGOOD <turbot2@hotmail.com>
2019-09-28 21:49:06 +0200

    Added controller and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do 
   devise_for :users, controllers: {registrations: 'users/registrations'}
+  resources :users do
+    resource :profile
+  end
   resources :contacts
   root 'pages#home'
   get '/about' => 'pages#about'
