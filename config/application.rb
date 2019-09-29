ed1d0b5e12b2874de2841e8ec59b4ae0d7c15476
MFISGOOD <turbot2@hotmail.com>
2019-09-29 23:27:48 +0200

    Added avatar column to Profile

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
-
 require 'rails/all'
+#Active Storage
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
